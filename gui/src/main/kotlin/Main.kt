f9560a92a562fc88f2a7700b19a1cfa84eb4b64f
kamex737 <kamex737@gmail.com>
2019-11-10 13:19:11 +0100

    added support for TornadoFx

--- gui/src/main/kotlin/Main.kt
+++ gui/src/main/kotlin/Main.kt
@@ -1,6 +1,11 @@
 package com.github.kbednarz.webscraper
 
-fun main() {
-    println("hello")
-    Example().test()
+import com.github.kbednarz.webscraper.component.MainWindow
+import tornadofx.App
+import tornadofx.launch
+
+class MainApp : App(MainWindow::class)
+
+fun main(args: Array<String>) {
+    launch<MainApp>(args)
 }
