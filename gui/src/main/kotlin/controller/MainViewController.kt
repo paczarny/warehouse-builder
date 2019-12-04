3545adbc66e9374ee5cce29f7f888be1587e5980
kamex737 <kamex737@gmail.com>
2019-12-04 22:57:49 +0100

    Implemented showing all entities (closes #4)

--- gui/src/main/kotlin/controller/MainViewController.kt
+++ gui/src/main/kotlin/controller/MainViewController.kt
@@ -1,48 +1,61 @@
 package pl.krakow.uek.wzisn2.etl.controller
 
 import javafx.beans.property.SimpleObjectProperty
+import javafx.beans.property.SimpleStringProperty
+import javafx.collections.FXCollections
+import javafx.collections.ObservableList
 import org.slf4j.LoggerFactory
+import pl.krakow.uek.wzisn2.etl.advert.Advert
 import pl.krakow.uek.wzisn2.etl.advert.AdvertService
 import pl.krakow.uek.wzisn2.etl.db.DatabaseConnector
 import tornadofx.*
 import java.lang.Thread.sleep
 
 class MainViewController : Controller() {
+    val advertsSize = SimpleStringProperty()
     private val logger = LoggerFactory.getLogger(MainViewController::class.java)
 
     private var advertService: AdvertService
     private var connector: DatabaseConnector = DatabaseConnector()
 
+    var adverts: ObservableList<Advert> = FXCollections.observableArrayList<Advert>()
+
     init {
         connector.start()
         advertService = AdvertService(connector)
+        refreshList()
     }
 
-    fun startEtl(it: SimpleObjectProperty<Pair<Long, Long>>) {
+    fun startEtl(progressProperty: SimpleObjectProperty<Pair<Long, Long>>) {
         logger.info("ETL process started")
         val url = "https://www.olx.pl/nieruchomosci/domy/krakow/"
         val lastPage = advertService.getLastPage(url)
 
         for (i in 1 until lastPage) {
             logger.info("Processing $i/$lastPage page")
-            it.set(Pair(i.toLong(), lastPage.toLong()))
+            updateProgress(progressProperty, i, lastPage)
             advertService.scrapListPage("$url?page=$i")
         }
-        it.set(Pair(lastPage.toLong(), lastPage.toLong()))
-
+        updateProgress(progressProperty, lastPage, lastPage)
         logger.info("End")
     }
 
-    fun startE(it: SimpleObjectProperty<Pair<Long, Long>>) {
+    private fun updateProgress(progressProperty: SimpleObjectProperty<Pair<Long, Long>>, i: Int, lastPage: Int, refresh: Boolean = true) {
+        runLater {
+            progressProperty.set(Pair(i.toLong(), lastPage.toLong()))
+            if (refresh) refreshList()
+        }
+    }
+
+    fun startE(progressProperty: SimpleObjectProperty<Pair<Long, Long>>) {
         logger.info("Extract process started")
-        val lastPage = 10L
+        val lastPage = 10
         for (i in 1 until lastPage) {
             logger.info("Processing $i/$lastPage page")
-            it.set(Pair(i, 10))
+            updateProgress(progressProperty, i, 10)
             sleep(1000)
         }
-        it.set(Pair(lastPage, lastPage))
-
+        updateProgress(progressProperty, lastPage, lastPage)
     }
 
     fun startT() {
@@ -51,5 +64,21 @@ class MainViewController : Controller() {
 
     fun startL() {
         println("Load process started")
+    }
+
+    fun clearDb(progressProperty: SimpleObjectProperty<Pair<Long, Long>>) {
+        val allAdverts = advertService.findAll()
+        for ((index, advert) in allAdverts.withIndex()) {
+            updateProgress(progressProperty, index, allAdverts.size, false)
+            advertService.delete(advert)
+        }
+        updateProgress(progressProperty, allAdverts.size, allAdverts.size)
+    }
+
+    private fun refreshList() {
+        val allAdverts = advertService.findAll()
+        adverts.clear()
+        adverts.addAll(allAdverts)
+        advertsSize.value = "${allAdverts.size} items"
     }
 }
--- gui/src/main/kotlin/view/MainView.kt
+++ gui/src/main/kotlin/view/MainView.kt
@@ -3,6 +3,7 @@ package pl.krakow.uek.wzisn2.etl.view
 import javafx.beans.property.SimpleBooleanProperty
 import javafx.scene.text.FontWeight
 import javafx.scene.text.TextAlignment
+import pl.krakow.uek.wzisn2.etl.advert.Advert
 import pl.krakow.uek.wzisn2.etl.controller.AppStylesheet
 import pl.krakow.uek.wzisn2.etl.controller.MainViewController
 import tornadofx.*
@@ -11,28 +12,46 @@ class MainView : View() {
     private val controller: MainViewController by inject()
     private val disable = SimpleBooleanProperty(false)
 
-    override val root = vbox {
-        addClass(AppStylesheet.mainView)
-        label {
-            text = "Web Scrapper"
-            style {
-                paddingBottom = 20
-                fontSize = 24.px
-                textAlignment = TextAlignment.CENTER
-                fontWeight = FontWeight.EXTRA_BOLD
+    override val root = borderpane {
+        left = vbox {
+            addClass(AppStylesheet.mainView)
+            label {
+                text = "Web Scrapper"
+                style {
+                    paddingBottom = 20
+                    fontSize = 24.px
+                    textAlignment = TextAlignment.CENTER
+                    fontWeight = FontWeight.EXTRA_BOLD
+                }
             }
+            add(ProgressButton("Run ETL process", disable) {
+                controller.startEtl(it)
+            })
+            add(ProgressButton("Run E process", disable) {
+                controller.startE(it)
+            })
+            add(ProgressButton("Run T process", disable) {
+                controller.startT()
+            })
+            add(ProgressButton("Run L process", disable) {
+                controller.startL()
+            })
+            add(ProgressButton("Clear database", disable) {
+                controller.clearDb(it)
+            })
         }
-        add(ProgressButton("Run ETL process", disable) {
-            controller.startEtl(it)
-        })
-        add(ProgressButton("Run E process", disable) {
-            controller.startE(it)
-        })
-        add(ProgressButton("Run T process", disable) {
-            controller.startT()
-        })
-        add(ProgressButton("Run L process", disable) {
-            controller.startL()
-        })
+
+        center = vbox {
+            style {
+                padding = box(20.px)
+            }
+            tableview<Advert> {
+                items = controller.adverts
+                column("Id", Advert::getId)
+                column("Price", Advert::getPrice)
+                column("Revision", Advert::getRevision)
+            }
+            label(controller.advertsSize)
+        }
     }
 }
